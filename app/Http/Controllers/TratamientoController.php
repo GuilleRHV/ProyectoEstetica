73db5d7dd880b0c06ceb8e80cc3f5fdf245b6ee9
AntonioGarciaCallejero <113922332+AntonioGarciaCallejero@users.noreply.github.com>
2023-02-21 08:41:51 +0100

    Update TratamientoController.php

--- app/Http/Controllers/TratamientoController.php
+++ app/Http/Controllers/TratamientoController.php
@@ -10,33 +10,20 @@ use Illuminate\Http\Request;
 //Tratamiento: Hay que darlos de alta para poder asignarlos a los socios.
 class TratamientoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+   
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     //Devuelve la vista create.
     public function create()
     {
         return view("tratamiento.create");
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+   
     //Almacena un tratamiento en la base de datos
     public function store(Request $request)
     {
@@ -50,7 +37,7 @@ class TratamientoController extends Controller
         return redirect()->route('esteticas.index')->with('exito', 'usuario creado correctamente');
     }
     
-    //una funcion para asignar el tratamiento con el socio
+    //Funcion para poder asignar un tratamiento con un socio.
     public function dartratamiento($id)
     {
         $socio = Socio::find($id);
@@ -59,46 +46,24 @@ class TratamientoController extends Controller
         return view('tratamiento.dartratamiento', ['socio' => $socio,'tratamientos' =>$tratamientos]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function destroy($id)
     {
         //
